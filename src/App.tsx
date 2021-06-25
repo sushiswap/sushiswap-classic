23ba00443b5e49b412cc2282945b55f565f7abc8
tomatecan <tomatecan@protonemail.com>
2021-06-25 09:18:47 -0400

    readded warning message

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ import Home from './views/Home'
 
 const App: React.FC = () => {
   const [mobileMenu, setMobileMenu] = useState(false)
+  const [showWarning, setShowWarning] = useState(true);
+
+
 
   const handleDismissMobileMenu = useCallback(() => {
     setMobileMenu(false)
@@ -31,7 +34,48 @@ const App: React.FC = () => {
     <Providers>
       <Router>
   
+      {showWarning &&
+          <div>
+            <div className="px-4 py-4 m-2 border-2 border-red-400 rounded-md shadow bg-red-50 sm:px-6 md:px-10 lg:px-32">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  {/* Heroicon name: x-circle */}
+                  <button
+                    onClick={evt => {
+                      evt.preventDefault();
+                      setShowWarning(false);
+                    }}
+                  >
+                    <svg
+                      className="w-8 h-8 text-red-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <div className="ml-3">
+                  <h3 className="text-sm font-medium text-red-800">
+                    TomatoToken is for entertainment purposes only. This application has not been audited. 
+                    You can lose your money in these types of applications.
+                  </h3>
 
+                  <h3 className="mt-4 text-sm font-semibold text-red-800">
+                    Use at your own risk.
+                  </h3>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        }
 
   
         <TopBar onPresentMobileMenu={handlePresentMobileMenu} />
